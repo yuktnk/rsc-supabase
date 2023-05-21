2d49db9e09bc5ad06d6e41d4ebf0fd86714e02ef
yuktnk <yuki0wl@gmail.com>
2023-05-21 20:08:36 +0900

    サーバーコンポーネントでのデータフェッチの挙動について

---
+++ app/components/notes-list.tsx
@@ -0,0 +1,47 @@
+import type { Database } from '../../database.types'
+import { format } from 'date-fns'
+
+type Note = Database['public']['Tables']['notes']['Row']
+
+async function fetchNotes() {
+  // await new Promise((resolve) => setTimeout(resolve, 2000))
+  const res = await fetch(`${process.env.url}/rest/v1/notes?select=*`, {
+    headers: new Headers({
+      apikey: process.env.apikey as string,
+    }),
+    // cache: 'no-store',
+    next: { revalidate: 10 },
+  })
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data in server')
+  }
+
+  const notes: Note[] = await res.json()
+
+  return notes
+}
+
+export default async function NotesList() {
+  const notes = await fetchNotes()
+
+  return (
+    <div>
+      <p className="my-4 pb-3 text-xl font-medium underline underline-offset-4">
+        Notes
+      </p>
+      <ul className="m-3">
+        {notes.map((note) => (
+          <li key={note.id}>
+            <p>{note.title}</p>
+            <p>
+              <b className="mr-3">Created at:</b>
+              {note && format(new Date(note.created_at), 'yyyy-MM-dd HH:mm:ss')}
+            </p>
+          </li>
+        ))}
+        <li></li>
+      </ul>
+    </div>
+  )
+}
